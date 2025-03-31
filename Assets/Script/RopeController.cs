cc8ae8c8d1c0a5695c27ebc4c62c03d41b4a6c6b
김도영 <jj0475442@gmail.com>
2025-03-31 18:04:32 +0900

    1
    1

--- Assets/Script/RopeController.cs
+++ Assets/Script/RopeController.cs
@@ -1,38 +1,42 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RopeController : MonoBehaviour
 {
-    //우주선과 플레이어의 길이
+    // 우주선과 플레이어의 줄 길이
     [SerializeField] public float ropeLength = 0f;
-    //루프의 최대 거리 
+    // 줄의 최대 거리
     [SerializeField] public float maxRopeLength = 50f;
-    //루프스타트
+    // 루프 시작점
     [SerializeField] public GameObject loopStart;
-    //루프엔드
+    // 루프 끝점
     [SerializeField] public GameObject loopEnd;
-    
+
     private void Update()
-    {   
+    {
         ropeLength = Vector3.Distance(loopStart.transform.position, loopEnd.transform.position);
 
         GameObject Player = GameManager.Instance.player.gameObject;
         Rigidbody playerRigidbody = Player.GetComponent<Rigidbody>();
+
+        Debug.Log(ropeLength);
         
         if (ropeLength >= maxRopeLength)
         {
             Vector3 directionBackToRope = (loopStart.transform.position - Player.transform.position).normalized;
+            
             Vector3 currentVelocity = playerRigidbody.linearVelocity;
             
-            if (Vector3.Dot(currentVelocity, directionBackToRope) < 0) 
+            if (Vector3.Dot(currentVelocity, directionBackToRope) < 0)
             {
-                playerRigidbody.linearVelocity = Vector3.zero; 
+                playerRigidbody.linearVelocity = Vector3.zero;
             }
-
-            GameManager.Instance.player.isMove = false;
+            else
+            {
+                GameManager.Instance.player.isMove = true;
+            }
         }
-        else if (ropeLength < maxRopeLength)
+        else
         {
             GameManager.Instance.player.isMove = true;
         }
