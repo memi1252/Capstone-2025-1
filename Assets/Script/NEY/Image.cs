1a0aa60b63e888d38423371cc69f275e5fa38c8b
naeunyu1027 <naeunyu081027@gmail.com>
2025-04-01 19:51:14 +0900

    test1

---
+++ Assets/Script/NEY/Image.cs
@@ -0,0 +1,16 @@
+using UnityEditor;
+using UnityEngine;
+
+public class Image : MonoBehaviour
+{
+    public GameObject Items;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
