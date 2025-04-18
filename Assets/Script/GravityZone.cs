1d56af203f59678481ae72f4c5107fc7f7bbe9b5
김도영 <jj0475442@gmail.com>
2025-04-18 11:37:10 +0900

    1
    1

--- Assets/Script/GravityZone.cs
+++ Assets/Script/GravityZone.cs
@@ -21,7 +21,7 @@ public class GravityZone : MonoBehaviour
             playerInsideCount++;
             if (playerInsideCount == 1)
             {
-                other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                other.gameObject.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
                 GameManager.Instance.isSpace = false;
                 GameManager.Instance.player.moveSpeed += movespeedAddORSub;
                 GameManager.Instance.player.thrustPower += thrustPowerAddORSub;
