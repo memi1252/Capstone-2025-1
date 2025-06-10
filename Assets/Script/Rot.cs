4b373554ea6075a4b4cd95e7770f05d0bede1f4d
memi0125 <jj0475442@gmail.com>
2025-06-10 11:14:53 +0900

    1
    1

--- Assets/Script/Rot.cs
+++ Assets/Script/Rot.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class Rot : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 50f;
+    public Vector3 rotationAxis = Vector3.up;
     
     
     private void Update()
     {
         // Rotate the object around its Y-axis at the specified speed
-        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime);
     }
 }
