0c855da38e314cba3aed6c1b33896dd072835f99
naeunyu1027 <naeunyu081027@gmail.com>
2025-06-22 02:58:33 +0900

    j

--- Assets/Script/Loby/DataManager.cs
+++ Assets/Script/Loby/DataManager.cs
@@ -2,35 +2,99 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 using System.IO;
-public class saveData
+using TMPro;
+public class SaveDatas
 {
     public Vector3 playerPosition;
+    public string fileName;
 }
 public class DataManager : MonoBehaviour
 {
-    public saveData SaveData = new saveData();
-    string jsonData;
+    public SaveDatas[] _saveData = new SaveDatas[3];
+    string[] jsonData = {null, null, null};
+
+    public TextMeshProUGUI _fileName;
+    public TextMeshProUGUI[] gameFileName;
+
+    string json;
+    string path;
 
     void Awake()
     {
-
+        for (int i = 0; i < 3; i++)
+        {
+            string path = Application.persistentDataPath + $"/save_{i}.json";
+            if (File.Exists(path))
+            {
+                string loadedJson = File.ReadAllText(path);
+                _saveData[i] = JsonUtility.FromJson<SaveDatas>(loadedJson);
+                jsonData[i] = loadedJson;
+            }
+        }
+        UpdateSlotFileNames();
     }
 
+    public void UpdateSlotFileNames()
+{
+    for (int i = 0; i < gameFileName.Length; i++)
+    {
+        path = Application.persistentDataPath + $"/save_{i}.json";
+
+        if (File.Exists(path))
+        {
+            json = File.ReadAllText(path);
+            SaveDatas temp = JsonUtility.FromJson<SaveDatas>(json);
+
+            if (temp != null && !string.IsNullOrEmpty(temp.fileName))
+                gameFileName[i].text = temp.fileName;
+            else
+                gameFileName[i].text = "빈 슬롯";
+        }
+        else
+        {
+            gameFileName[i].text = "빈 슬롯";
+        }
+    }
+}
+
+    int fileNumber;
     public void GameSave()
     {
-        jsonData = JsonUtility.ToJson(SaveData);
+       _saveData[fileNumber] = new SaveDatas()
+        {
+            fileName = _fileName.text,
+        };
+
+        jsonData[fileNumber] = JsonUtility.ToJson(_saveData[fileNumber]);
+        path = Application.persistentDataPath + $"/save_{fileNumber}.json";
+        File.WriteAllText(path, jsonData[fileNumber]);
+        Debug.Log($"슬롯 {fileNumber} 저장 완료 at {path}");
     }
+
     public void Gameload()
     {
-
-    }
-    public GameObject fileName;
-    public void cheak()
-    {
-        if (jsonData == null)
+        path = Application.persistentDataPath + $"/save_{fileNumber}.json";
+        if (File.Exists(path))
         {
-            fileName.SetActive(true);
+            string loadedJson = File.ReadAllText(path);
+            _saveData[fileNumber] = JsonUtility.FromJson<SaveDatas>(loadedJson);
+            Debug.Log($"로드 완료: {_saveData[fileNumber].fileName}");
         }
+        else
+        {
+            fileNameInputField.SetActive(true);
+            Debug.LogWarning("저장된 파일이 없습니다.");
+        }
+    }
+    public GameObject fileNameInputField;
+    public void cheak(int num)
+    {
+        
+        fileNumber = num;
+        Debug.Log("num = " + num);
+        Debug.Log("filenumber = " + fileNumber);
+        Gameload();
+
     }
 
     public void nameClear()
