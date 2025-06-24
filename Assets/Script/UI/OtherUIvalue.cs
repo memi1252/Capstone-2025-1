553568805a7bebe7d985294bcdfcc44c0b3cad15
naeunyu1027 <naeunyu081027@gmail.com>
2025-06-25 00:56:46 +0900

    j

---
+++ Assets/Script/UI/OtherUIvalue.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OtherUIvalue : MonoBehaviour
+{
+    public Slider fatigueSlider;
+    public Slider HPSlider;
+    void Start()
+    {
+        StartCoroutine(FatigueBar());
+        StartCoroutine(HPbar());
+    }
+
+    void Update()
+    {
+        
+    }
+    IEnumerator FatigueBar()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(6f);
+            Gague();
+        }
+    }
+
+    IEnumerator HPbar()
+    {
+        while (true)
+        {
+            if (fatigueSlider.GetComponent<Slider>().value <= 90)
+            {
+                yield return new WaitForSeconds(7f);
+                HPSlider.GetComponent<Slider>().value -= 5;
+            }
+        }
+    }
+
+    void Gague()   
+    {
+        fatigueSlider.GetComponent<Slider>().value -= 7;
+        // if (fatigueSlider.GetComponent<Slider>().value <= 90)
+        // {
+        //     StartCoroutine(HPbar());
+        // }
+    }
+}
