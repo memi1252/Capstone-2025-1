c56a0dcf3544a4e5a3c39934b554d6351b814f9a
naeunyu1027 <naeunyu081027@gmail.com>
2025-06-25 17:28:44 +0900

    d

--- Assets/Script/UI/OtherUIvalue.cs
+++ Assets/Script/UI/OtherUIvalue.cs
@@ -6,13 +6,31 @@ public class OtherUIvalue : MonoBehaviour
 {
     public Slider fatigueSlider;
     public Slider HPSlider;
+    public Slider Oxy1;
+    public Slider Oxy2;
     void Start()
     {
     }
 
     void Update()
     {
-        fatigueSlider.GetComponent<Slider>().value -= Time.deltaTime * 1;
+        fatigueSlider.GetComponent<Slider>().value -= Time.deltaTime * 0.4f;
+        if (fatigueSlider.GetComponent<Slider>().value <= 0)
+        {
+            HPSlider.GetComponent<Slider>().value -= Time.deltaTime * 3;
+        }
+
+        if (GameManager.Instance.inSpaceShip == false)
+        {
+            if (Oxy1.GetComponent<Slider>().value > 0)
+            {
+                Oxy1.GetComponent<Slider>().value -= Time.deltaTime * 0.4f;
+            }
+            else
+            {
+                Oxy2.GetComponent<Slider>().value -= Time.deltaTime * 0.4f;
+            }
+        }
     }
     
 }
