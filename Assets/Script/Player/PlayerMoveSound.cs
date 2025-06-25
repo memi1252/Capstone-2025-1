e596d7998a1de90dd681b295ac50f1cd4c063674
naeunyu1027 <naeunyu081027@gmail.com>
2025-06-26 08:53:54 +0900

    k

--- Assets/Script/Player/PlayerMoveSound.cs
+++ Assets/Script/Player/PlayerMoveSound.cs
@@ -2,10 +2,25 @@ using UnityEngine;
 
 public class PlayerMoveSound : MonoBehaviour
 {
-    
+    public AudioClip walkSound;
+    public AudioClip runSound;
+    public AudioClip jumpSound;
+    public AudioSource playerAudioSource;
 
     void Update()
     {
-        
+
+    }
+    void WalkSound()
+    {
+        playerAudioSource.PlayOneShot(walkSound);
+    }
+    void RunSound()
+    {
+        playerAudioSource.PlayOneShot(runSound);
+    }
+    void JumpSound()
+    {
+        playerAudioSource.PlayOneShot(jumpSound);
     }
 }
