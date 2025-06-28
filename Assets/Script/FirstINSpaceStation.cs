cab90b3b8eeee315b6836b04fa460a5eb040ffb2
naeunyu1027 <naeunyu081027@gmail.com>
2025-06-28 22:45:24 +0900

    이거맞나?

--- Assets/Script/FirstINSpaceStation.cs
+++ Assets/Script/FirstINSpaceStation.cs
@@ -67,9 +67,9 @@ public class FirstINSpaceStation : MonoBehaviour
         yield return new WaitForSeconds(2f);
         var dialogTexts = new List<DialogData>();
         isPlay = true;
-        dialogTexts.Add(new DialogData("우주정거장 스캔 결과 도구를 만들 재료는 충분합니다."));
-        dialogTexts.Add(new DialogData("현재 필요한 재료는 철판, 나사, 볼트입니다."));
-        dialogTexts.Add(new DialogData("피로도가 다 떨어지기 전에 모아서 우주선으로 돌아갑시다!"));
+        dialogTexts.Add(new DialogData("우주정거장 스캔 결과 도구를 만들 재료는 충분합니다"));
+        dialogTexts.Add(new DialogData("현재 필요한 재료는 철판, 나사, 볼트입니다"));
+        dialogTexts.Add(new DialogData("피로도와 산소가 다 떨어지기 전에 모아서 우주선으로 복귀해주세요"));
         GameManager.Instance.BBASS.Show(dialogTexts);
     }
 
@@ -78,10 +78,10 @@ public class FirstINSpaceStation : MonoBehaviour
         yield return new WaitForSeconds(2f);
         var dialogTexts = new List<DialogData>();
         isPlay = true;
-        dialogTexts.Add(new DialogData("스캔 결과 재료가 좀 깊숙히 있습니다."));
-        dialogTexts.Add(new DialogData("근데 현재 거기로 가는 길이 잠겨있습니다."));
-        dialogTexts.Add(new DialogData("카드키가 있으면 열릴꺼 같습니다."));
-        dialogTexts.Add(new DialogData("카드키를 찾아서 우주정거장을 탐색해봅시다."));
+        dialogTexts.Add(new DialogData("스캔 결과 좀 더 안쪽으로 들어가야 할 것 같습니다"));
+        dialogTexts.Add(new DialogData("만약 잠긴문이 있다면 카드키를 찾아 열어야 합니다"));
+        dialogTexts.Add(new DialogData("카드키가 있으면 열릴꺼 같습니다"));
+        dialogTexts.Add(new DialogData("카드키를 찾아서 우주정거장을 탐색해봅시."));
         GameManager.Instance.BBASS.Show(dialogTexts);
     }
 
