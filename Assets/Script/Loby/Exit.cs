e1aaae2923b4165c1b00127e60f0d1925ffb1244
memi1252 <jj0475442@gmail.com>
2025-06-29 17:36:59 +0900

    사운드 이이얏

--- Assets/Script/Loby/Exit.cs
+++ Assets/Script/Loby/Exit.cs
@@ -7,8 +7,14 @@ public class Exit : MonoBehaviour
     {
 
 #if UNITY_EDITOR
+        PlayerPrefs.SetFloat("BackgroundVolume", SoundManager.Instance.BackgroundSlider.value);
+        PlayerPrefs.SetFloat("MasterVolume", SoundManager.Instance.MasterSlider.value);
+        PlayerPrefs.SetFloat("EffectVolume", SoundManager.Instance.EffectSlider.value);
         UnityEditor.EditorApplication.isPlaying = false;  // 에디터에서 실행 중지
 #else
+        PlayerPrefs.SetFloat("BackgroundVolume", SoundManager.Instance.BackgroundSlider.value);
+        PlayerPrefs.SetFloat("MasterVolume", SoundManager.Instance.MasterSlider.value);
+        PlayerPrefs.SetFloat("EffectVolume", SoundManager.Instance.EffectSlider.value);
         Application.Quit();  // 빌드된 게임 종료
 #endif
     }
